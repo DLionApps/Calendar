34b4f8fc7dca4810ab5f8b1fc89c0eb527323159
DLionAppsPersonal <63153805+DLionApps@users.noreply.github.com>
2020-05-31 17:16:17 +0530

    Update LandingMonthDetails.js
    Added new static objects

--- assets/staticFiles/LandingMonthDetails.js
+++ assets/staticFiles/LandingMonthDetails.js
@@ -9,4 +9,20 @@ let monthArray = [
   weekArray,
 ];
 
-export default monthArray;
+const dayArray = [
+  { name: "MON" },
+  { name: "TUE" },
+  { name: "WED" },
+  { name: "THU" },
+  { name: "FRI" },
+  { name: "SAT" },
+  { name: "SUN" },
+];
+
+const monthDetail = {
+  currentMonth: "Month loading",
+  currentYear: "Year loading",
+  currentMonthNumber: 0,
+};
+
+export { monthArray, monthDetail, dayArray };
