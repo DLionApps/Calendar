657abf9269331b7e6ab86507b3f6a582b3d4106a
DLionAppsPersonal <63153805+DLionApps@users.noreply.github.com>
2020-05-31 17:28:07 +0530

    Create Memo.js
    Added new hook

---
+++ hooks/Memo.js
@@ -0,0 +1,9 @@
+import React, { useState, useContext, useEffect } from "react";
+
+const useMemoAdd = () => {
+  const [newMemo, setNewMemo] = useState();
+};
+
+const useMemoGet = () => {};
+
+export { useMemoAdd, useMemoGet };
